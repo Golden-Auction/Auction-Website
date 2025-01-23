ec96e8825f0655d8a845578366b7ea06787ced3e
yared <yared@gmail.com>
2025-01-23 05:10:14 -0900

    modify footer icon section

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,5 +1,22 @@
-function goToSignInPage(){
-    document.addEventListener("click",function (){
-        location.replace("../HTML/signin.html");
-    });
-}
+/*********  Sign In / Sign Up pages *********/
+// Select the buttons and forms
+const signinBtn = document.getElementById('signin-btn');
+const signupBtn = document.getElementById('signup-btn');
+const signinForm = document.getElementById('signin-form');
+const signupForm = document.getElementById('signup-form');
+
+// Add event listeners to the buttons
+signinBtn.addEventListener('click', () => {
+  signinForm.classList.add('active');
+  signupForm.classList.remove('active');
+  signinBtn.classList.add('active');
+  signupBtn.classList.remove('active');
+});
+
+signupBtn.addEventListener('click', () => {
+  signupForm.classList.add('active');
+  signinForm.classList.remove('active');
+  signupBtn.classList.add('active');
+  signinBtn.classList.remove('active');
+});
+/*********  Sign In / Sign Up pages *********/
